f667df0cf291db4f98d7fa0761fadcab6f0f5ca6
TyDooo <tydooo@fastmail.com>
2026-03-12 19:38:19 +0100

    hosts/zoltraak: setup network bond and systemd networking

--- hosts/zoltraak/networking.nix
+++ hosts/zoltraak/networking.nix
@@ -1,12 +1,42 @@
-{
-  config,
-  lib,
-  ...
-}: {
+{config, ...}: {
   networking = {
-    useDHCP = lib.mkDefault true;
-    networkmanager.enable = true;
-    hostId = "1f4d827b";
+    firewall.enable = true;
+    networkmanager.enable = false;
+    hostId = "1f4d827b"; # IDK? For ZFS apparently...
+    useDHCP = false;
+  };
+
+  systemd.network.enable = true;
+  systemd.network = {
+    netdevs = {
+      "10-bond0" = {
+        netdevConfig = {
+          Kind = "bond";
+          Name = "bond0";
+        };
+        bondConfig = {
+          Mode = "802.3ad";
+          TransmitHashPolicy = "layer3+4";
+        };
+      };
+    };
+    networks = {
+      "30-enp2s0" = {
+        matchConfig.Name = "enp2s0";
+        networkConfig.Bond = "bond0";
+        linkConfig.RequiredForOnline = "no";
+      };
+      "30-eno1" = {
+        matchConfig.Name = "eno1";
+        networkConfig.Bond = "bond0";
+        linkConfig.RequiredForOnline = "no";
+      };
+      "40-bond0" = {
+        matchConfig.Name = "bond0";
+        linkConfig.RequiredForOnline = "routable";
+        networkConfig.DHCP = "yes";
+      };
+    };
   };
 
   vpnNamespaces.proton0 = {
